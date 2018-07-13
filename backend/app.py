f9bf7ad3f022002a2ea961858a3f733bd293c320
Waihon Chee <imwaihon@gmail.com>
2018-07-14 00:19:54 +0800

    Implement naive fact checker

---
+++ backend/app.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, jsonify
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+import requests
+import syntax_triples.main
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return 'Einherjar Online.'
+
+@app.route('/parse', methods=['POST'])
+def parse():
+    return jsonify({
+        'factCheck': parseFactCheck(request.json['textPayload']) 
+    })
+
+def parseFactCheck(text_payload):
+    # Get triplets through syntax_triples
+    triplets = syntax_triples.main.parseText(text_payload)
+    for i in range(0, len(triplets)):
+        # Check factually to fact check
+        teamURL = "https://www.gov.sg/factually/all/"+triplets[i][2]+"/page-1"
+        
+        headers = requests.utils.default_headers()
+        headers.update({
+            'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0',
+        })
+
+        link = requests.get(teamURL, headers=headers)
+
+        soup = BeautifulSoup(link.content, 'html.parser')
+        newsitems = soup.select('.noresults')
+        if not newsitems:
+            return True
+        return False
+
+def parseSentimentAnalysis(text_payload):
+    print('lol')
